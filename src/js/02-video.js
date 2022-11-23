3817c01588c97e3c75c5123d00b41062401ab952
NikitaSimakov <mishaaniskin@gmail.com>
2022-11-23 21:22:08 +0300

    second task

---
+++ src/js/02-video.js
@@ -0,0 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+// console.log(Player);
+
+const iframe = document.querySelector('iframe', throttle(onPlay, 1000));
+const player = new Player(iframe);
+
+function onPlay({ seconds }) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+    
+}
+console.log(localStorage.getItem('videoplayer-current-time'));
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')));
+// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
