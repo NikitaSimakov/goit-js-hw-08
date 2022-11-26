bd71c90da91ddf8c4dc59ab445141642a231302d
NikitaSimakov <mishaaniskin@gmail.com>
2022-11-26 16:13:57 +0300

    Update 03.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,46 +1,3 @@
-// import throttle from "lodash.throttle";
-// const refs = {
-//     form: document.querySelector('.feedback-form'),
-//     textarea: document.querySelector('.feedback-form textarea'),
-
-// }
-// const userData = {
-//     'email' : '',
-//     'message' : ''
-// }
-// const STORAGE_KEY = 'feedback-form-state';
-// populateTextarea();
-
-// // console.log(refs.textarea);
-
-// refs.form.addEventListener('submit', onFormSubmit);
-
-// refs.textarea.addEventListener('input', throttle(onTextAreaInput, 500));
-
-// function onTextAreaInput(evt) {
-//     const message = evt.target.value;
-
-//     localStorage.setItem(STORAGE_KEY, message);
-// }
-
-// function onFormSubmit(evt) {
-//     evt.preventDefault();
-
-//     console.log('Sending form');
-
-//     evt.target.reset();
-//     localStorage.removeItem(STORAGE_KEY);
-// }
-
-// function populateTextarea() {
-//     const savedMessage = localStorage.getItem(STORAGE_KEY);
-
-//     if (savedMessage) {
-//         console.log(savedMessage);
-//         refs.textarea.value = savedMessage;
-//     }
-// }
-
 import throttle from "lodash.throttle";
 const refs = {
     form: document.querySelector('.feedback-form'),
@@ -86,3 +43,46 @@ function populateTextarea() {
       });
     }
 }
+
+// import throttle from "lodash.throttle";
+// const refs = {
+//     form: document.querySelector('.feedback-form'),
+//     textarea: document.querySelector('.feedback-form textarea'),
+
+// }
+// const userData = {
+//     'email' : '',
+//     'message' : ''
+// }
+// const STORAGE_KEY = 'feedback-form-state';
+// populateTextarea();
+
+// // console.log(refs.textarea);
+
+// refs.form.addEventListener('submit', onFormSubmit);
+
+// refs.textarea.addEventListener('input', throttle(onTextAreaInput, 500));
+
+// function onTextAreaInput(evt) {
+//     const message = evt.target.value;
+
+//     localStorage.setItem(STORAGE_KEY, message);
+// }
+
+// function onFormSubmit(evt) {
+//     evt.preventDefault();
+
+//     console.log('Sending form');
+
+//     evt.target.reset();
+//     localStorage.removeItem(STORAGE_KEY);
+// }
+
+// function populateTextarea() {
+//     const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+//     if (savedMessage) {
+//         console.log(savedMessage);
+//         refs.textarea.value = savedMessage;
+//     }
+// }
